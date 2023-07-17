2bf444d047bf63d56658db80704f2db0962b4a9f
ayushtiw <kamleshtiwary753@gmail.com>
2023-07-17 19:40:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0bbdd77f15mshbb3790cb9bda0f5p17e1f3jsnb9e59a605a73',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+
+const getWeather = (city)=>{
+cityName.innerHTML = city;
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city;
+fetch(url,options)
+.then(response => response.json())
+.then(response => {
+    console.log(response)
+
+    
+
+  
+cloud_pct.innerHTML = response.cloud_pct
+temp.innerHTML = response.temp
+temp2.innerHTML = response.temp
+feels_like.innerHTML = response.feels_like
+humidity.innerHTML = response.humidity
+humidity2.innerHTML = response.humidity
+min_temp.innerHTML = response.min_temp
+max_temp.innerHTML = response.max_temp
+wind_speed.innerHTML = response.wind_speed
+wind_speed2.innerHTML = response.wind_speed
+// wind_degrees.innerHTML = response.wind_degrees
+sunrise.innerHTML = response.sunrise
+sunset.innerHTML = response.sunset
+
+    
+})
+.catch(err => console.log(err));
+
+
+}
+
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    getWeather(city.value);
+})
+
+getWeather("Delhi");
+
+
